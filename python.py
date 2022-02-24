debd0ec64504342c2d7bfbecb7c8d53a82d63c47
Bruno Nasser <brunonasser@yahoo.com.br>
2022-02-23 22:53:11 -0300

    com codigo de download

--- python.py
+++ python.py
@@ -2,3 +2,11 @@
 
 print('hello world')
 
+import urllib.request
+
+print('Beginning file download with urllib2...')
+
+url = 'http://200.152.38.155/CNPJ/K3241.K03200Y0.D20212.EMPRECSV.zip'
+urllib.request.urlretrieve(url, '/Users/nasse/Downloads/teste2')
+
+
